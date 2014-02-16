6de30af9688689136d1c870ff702dd5a1c91a79d
Simms Richard <Richard@Simms-Richards-MacBook-Pro.local>
2014-02-16 20:08:57 +1100

    Pushing all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GentlemanRaptor::Application.config.secret_key_base = '1988f85c1a3dca19282b5fd0b0ee02a1cdb811f01180a2d76cb79f232dd5650cfa663f4c180c87ca5a18dc4ccd45c677cce610a857d7131a33be9b55a7149ee2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GentlemanRaptor::Application.config.session_store :cookie_store, key: '_gentleman_raptor_session'
